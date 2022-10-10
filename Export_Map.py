5a86af02ae788ea01b17c49822df21f821c9bcca
Tuyet Anh <84496906+tuyetanh0207@users.noreply.github.com>
2022-10-10 21:59:35 +0700

    first idea

---
+++ Export_Map.py
@@ -0,0 +1,8 @@
+def export_map_Normal_BFS():
+    with open ("Normal_BFS","w+") as f:
+        f.write("xxxxxxxxxxxxxxxx\n")
+        f.write("xxxxxxxxxxxxxxxx")
+        f.write("xxxxxxxxxxxxxxxx")
+        f.write("xxxxxxxxxxxxxxxx")
+        f.write("xxxxxxxxxxxxxxxx")
+export_map_Normal_BFS()
---
+++ Star_BFS.py
@@ -0,0 +1,8 @@
+# coi mỗi điểm là 1 đơn vị trên hệ tọa độ x,y
+
+
+
+# cánh cửa là điểm đích
+
+# sau khi đọc file
+# chuyển thành mảng 2 chiều, 1 0 2
