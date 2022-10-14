76f7d16570a048841b80737b7346671c2c5bc4ba
IowaNguyen <95394142+IowaNguyen@users.noreply.github.com>
2022-10-14 09:03:55 +0700

    Add files via upload

--- Normal_DFS.py
+++ Normal_DFS.py
@@ -1,1 +1,35 @@
-import os
+import copy
+
+def Normal_DFS(maxtrix, start, end):
+    temp_maxtrix = copy.deepcopy(maxtrix)
+    parent_save = copy.deepcopy(maxtrix)
+    stack = [start]
+    d = 0
+    
+    while len(stack) != 0:
+        d += 1
+        current = stack.pop()
+        temp_maxtrix[current[0]][current[1]] = 'v'
+        
+        if current == end:
+            break
+
+        arounds = [(current[0] - 1, current[1]), (current[0], current[1] - 1),
+                   (current[0] + 1, current[1]), (current[0], current[1] + 1)]
+
+        for i in arounds:
+            if (temp_maxtrix[i[0]][i[1]] != 'v' and maxtrix[i[0]][i[1]] != 'x'):
+                parent_save[i[0]][i[1]] = current
+                stack.append(i)
+        
+    route = [end]
+    point = end
+    while point != start:
+        route.insert(0, parent_save[point[0]][point[1]])
+        point = parent_save[point[0]][point[1]]
+
+    print('So vong lap DFS:', d)
+    return route
+
+
+
